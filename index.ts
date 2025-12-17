78cc8c96295241ccb86416338a46a57dd1f8ef50
Nikhil Yadav <facts.foundr@gmail.com>
2025-12-17 12:31:49 +0530

    add server listening again
    Add error handling for database connection failure

--- index.ts
+++ index.ts
@@ -25,5 +25,14 @@ app.use('/api/v1/brain', authMiddleware , brainRouter)
 
 
 ConnectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`server running on Port`,PORT );
+        });
+    })
+    .catch((error) => {
+        console.error('Failed to connect to the database:', error);
+    });
+
 
 export default app;
